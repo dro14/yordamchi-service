881aa7495c9e5110a91a52a8dae090086fd4e2da
dro14 <doniyorbek752@gmail.com>
2023-11-05 09:42:56 +0500

    restructure the code

--- Dockerfile
+++ Dockerfile
@@ -15,8 +15,9 @@ WORKDIR /app
 
 COPY . .
 
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --upgrade pip && \
+    pip install --no-cache-dir -r requirements.txt
 
 EXPOSE 8000
 
-CMD ["python", "main.py"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- main.py
+++ main.py
@@ -1,33 +1,11 @@
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver import Firefox, FirefoxOptions
-from selenium.webdriver.common.by import By
+from functions import google_search, clean_data
 from fastapi import FastAPI, Request
-from urllib.parse import quote
-from uvicorn import run
-from re import match
-
-REDUNDANT_LINES = (
-    r"http",
-    r"·",
-    r"Translate this page",
-    r"•",
-    r"Feedback",
-    r"Missing",
-    r"Rating",
-    r"\d+ answers",
-    r"Top answer:",
-    r"^(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s([1-9]|[12][0-9]|3[01]),\s\d{4}$",
-)
-
-options = FirefoxOptions()
-options.add_argument("--headless")
-driver = Firefox(options=options)
-driver.implicitly_wait(1)
-
-app_fastapi = FastAPI()
 
 
-@app_fastapi.post("/search")
+app = FastAPI()
+
+
+@app.post("/search")
 async def search_endpoint(request: Request):
     data = await request.json()
     query = data["query"]
@@ -37,56 +15,3 @@ async def search_endpoint(request: Request):
         return {"results": results}
     else:
         return {"error": "No query provided"}
-
-
-def main():
-    run(app_fastapi)
-
-
-def make_url(query):
-    return "https://www.google.com/search?&hl=en&num=10&q=" + quote(query)
-
-
-def clean_data(elements):
-    results = []
-    for element in elements:
-        redundant_elements = ("People also ask", "Related searches", "Related search", "Images", "Videos")
-        if not element.text or element.text.startswith(redundant_elements):
-            continue
-
-        lines = element.text.splitlines()
-
-        i = 0
-        while i < len(lines):
-            lines[i] = lines[i].strip()
-            for redundant_line in REDUNDANT_LINES:
-                if match(redundant_line, lines[i]):
-                    lines.pop(i)
-                    i -= 1
-                    break
-            i += 1
-
-        try:
-            link = element.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
-        except NoSuchElementException:
-            link = None
-
-        if link:
-            lines.append(link)
-
-        results.append("\n".join(lines))
-
-    return "\n\n".join(results)
-
-
-def google_search(query):
-    driver.get(make_url(query))
-    try:
-        driver.find_element(By.ID, "L2AGLb").click()
-    except NoSuchElementException:
-        pass
-    return driver.find_elements(By.CLASS_NAME, "MjjYud")
-
-
-if __name__ == '__main__':
-    main()
