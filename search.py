1d7d022d63d9f72708a42437206c5a9478bc17c1
dro14 <doniyorbek752@gmail.com>
2024-08-03 01:19:50 +0500

    edit the json format of search results

--- search.py
+++ search.py
@@ -71,6 +71,6 @@ def search(query: str, lang: str) -> str:
         else:
             i += 1
 
-    results = json.dumps(results, ensure_ascii=False)
-    print(f"QUERY: {query}, RESULTS: {results}")
+    results = json.dumps(results, ensure_ascii=False, indent=4)
+    print(f"QUERY: {query}, RESULTS:\n{results}")
     return results
