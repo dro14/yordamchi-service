7e927835e2deff36373dfd510ab30a7fbe72a05c
dro14 <doniyorbek752@gmail.com>
2024-08-03 02:35:09 +0500

    add the first result

--- search.py
+++ search.py
@@ -24,6 +24,13 @@ driver = webdriver.Firefox(options=options)
 driver.implicitly_wait(0.5)
 
 
+def get_title(element) -> str:
+    try:
+        return element.find_element("css selector", "h3").text
+    except NoSuchElementException:
+        return ""
+
+
 def get_text(element, class_name) -> str:
     try:
         return element.find_element("class name", class_name).text
@@ -39,7 +46,7 @@ def get_url(element) -> str:
 
 
 def search(query: str, lang: str) -> str:
-    url = f"https://www.google.com/search?hl={lang}&gl=uz&num=10&q={quote(query)}"
+    url = f"https://www.google.com/search?hl={lang}&gl=uz&num=3&q={quote(query)}"
     driver.get(url)
 
     try:
@@ -54,21 +61,37 @@ def search(query: str, lang: str) -> str:
         except NoSuchElementException:
             continue
         results.append({
+            "title": get_title(element),
             classes[class_name][1]: get_text(element, classes[class_name][0]),
             "url": get_url(element),
         })
 
+    first_result = driver.find_element("class name", "MjjYud")
+    results.append({
+        "title": get_title(first_result),
+        "result": first_result.text,
+        "url": get_url(first_result),
+    })
+
     for element in driver.find_elements("class name", "MjjYud"):
         results.append({
+            "title": get_title(element),
             "result": get_text(element, "VwiC3b"),
             "url": get_url(element),
         })
+        if results[-1]["url"] == get_url(first_result):
+            results.pop()
 
     i = 0
     while i < len(results):
-        if "result" in results[i] and not results[i]["result"]:
+        if (not results[i].get("featured_snippet", "") and
+                not results[i].get("description", "") and not results[i].get("result", "")):
             results.pop(i)
         else:
+            if not results[i]["title"]:
+                results[i].pop("title")
+            if not results[i]["url"]:
+                results[i].pop("url")
             i += 1
 
     return json.dumps(results, ensure_ascii=False, indent=4)
