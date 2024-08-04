593fc2a18502b3980197a3ee951daa706965d548
dro14 <doniyorbek752@gmail.com>
2024-08-04 14:36:22 +0500

    add space between parts

--- search.py
+++ search.py
@@ -19,22 +19,22 @@ classes = {
 
 
 def get_title(element) -> str:
-    h3 = element.find("h3")
-    return h3.text if h3 else ""
+    element = element.find("h3")
+    return element.get_text(separator=" ", strip=True) if element else ""
 
 
 def get_text(element, class_name) -> str:
-    div = element.find("div", class_=class_name)
-    return div.text if div else ""
+    element = element.find("div", class_=class_name)
+    return element.get_text(separator=" ", strip=True) if element else ""
 
 
 def get_url(element) -> str:
     a = element.find("a")
-    return a["href"] if a and "href" in a else ""
+    return a.get("href", "") if a else ""
 
 
 def search(query: str, lang: str) -> str:
-    url = f"https://www.google.com/search?hl={lang}&gl=uz&num=3&q={quote(query)}"
+    url = f"https://www.google.com/search?hl={lang}&gl=uz&num=5&q={quote(query)}"
     response = requests.get(url, headers={"User-Agent": UserAgent().random})
     soup = BeautifulSoup(response.text, "html.parser")
 
@@ -54,7 +54,7 @@ def search(query: str, lang: str) -> str:
         if not element.find("div", class_="VwiC3b"):
             results.append({
                 "title": get_title(element),
-                "result": element.text,
+                "result": element.get_text(separator=" ", strip=True),
                 "url": get_url(element),
             })
             do_summary = False
